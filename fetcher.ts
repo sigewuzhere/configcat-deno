6908399ab712e3a5fb4d53c525070cdd973c9a4e
sige <sige@configcat.com>
2020-05-29 02:25:42 +0200

    making it work

--- fetcher.ts
+++ fetcher.ts
@@ -1,7 +1,7 @@
-
 import { ProjectConfig } from "./common/ProjectConfig.ts";
 import { OptionsBase } from "./common/ConfigCatClientOptions.ts";
 import { soxa } from 'https://deno.land/x/soxa/mod.ts'
+import { IConfigFetcher } from "./common/index.ts";
 
 
 export class HttpConfigFetcher implements IConfigFetcher {
@@ -9,18 +9,15 @@ export class HttpConfigFetcher implements IConfigFetcher {
 
     fetchLogic(options: OptionsBase, lastProjectConfig: ProjectConfig, callback: (newProjectConfig: ProjectConfig) => void): void {
 
-        soxa.get(options.getUrl()
-        // , {
-        //     headers: {
-        //         "User-Agent": "ConfigCat-Node/" + options.clientVersion,
-        //         "X-ConfigCat-UserAgent": "ConfigCat-Node/" + options.clientVersion,
-        //         "If-None-Match": (lastProjectConfig && lastProjectConfig.HttpETag) ? lastProjectConfig.HttpETag : null
-        //     }
-        // }
+        soxa.get(options.getUrl(), {
+            headers: {
+                "User-Agent": "ConfigCat-Deno/" + options.clientVersion,
+                "X-ConfigCat-UserAgent": "ConfigCat-Deno/" + options.clientVersion,
+                "If-None-Match": (lastProjectConfig && lastProjectConfig.HttpETag) ? lastProjectConfig.HttpETag : null
+            }
+        }
         ).then((response:any) => {
-            if (response && response.status === 304) {
-                callback(new ProjectConfig(new Date().getTime(), JSON.stringify(lastProjectConfig.ConfigJSON), response.headers.etag as string));
-            } else if (response && response.status === 200) {
+            if (response && response.status === 200) {
                 callback(new ProjectConfig(new Date().getTime(), JSON.stringify(response.data), response.headers.etag as string));
             } else {
                 options.logger.error(`1Failed to download feature flags & settings from ConfigCat. Status: ${response && response.status} - ${response && response.statusText}`);
@@ -40,4 +37,4 @@ export class HttpConfigFetcher implements IConfigFetcher {
     }
 }
 
-export default IConfigFetcher;
+export default HttpConfigFetcher;
