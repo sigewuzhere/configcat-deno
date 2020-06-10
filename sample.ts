52fe45e9cb74d98ea94f032e981eb4c1267e12ea
sige <sige@configcat.com>
2020-06-10 13:40:46 +0200

    sample

--- sample.ts
+++ sample.ts
@@ -1,3 +1,5 @@
+// Run: deno run --allow-net sample.ts
+
 import * as configcat from "https://raw.githubusercontent.com/sigewuzhere/configcat-deno/master/client.ts";
 
 const logger = configcat.createConsoleLogger(3); // Setting log level to 3 (= Info) to show detailed feature flag evaluation
@@ -13,4 +15,3 @@ const userObject = { identifier: "#SOME-USER-ID#", email: "configcat@example.com
 
 const isPOCFeatureEnabled = await configCatClient.getValueAsync("isPOCFeatureEnabled", false, userObject);
 console.warn("isPOCFeatureEnabled: " + isPOCFeatureEnabled);
-
